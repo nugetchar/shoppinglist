bf98d8248f825335c62cce0af8990c450c7b348a
Thomas Toledo <nugetchar@gmail.com>
2018-12-23 22:50:26 +0100

    added form control

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,51 +1,91 @@
 import React from 'react';
+import { ValidatorForm } from 'react-form-validator-core';
+import TextValidator from '../validators/TextValidator';
 
  class Form extends React.Component {
-     state = {
-         name: '',
-         quantity: 0,
-     };
+    DEFAULT_STATE = {
+        article: {
+            name: 'toto',
+            quantity: 0
+        }
+    };
 
-     DEFAULT_STATE = {
-         name: '',
-         quantity: 0
-     };
+    constructor(props) {
+        super(props);
+        this.state = this.DEFAULT_STATE;
+
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeQuantity = this.handleChangeQuantity.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
      render() {
          return (
              <div>
                  <h3>{this.props.formTitle}</h3>
-                 <form onSubmit={this.handleSubmit} className="form-group">
+                 <ValidatorForm 
+                 onSubmit={this.handleSubmit}
+                 className="form-group">
+                    
+                    <TextValidator
+                        onChange={this.handleChangeName}
+                        type="text"
+                        name="itemName"
+                        placeholder="Nom de l'article" 
+                        className="form-control"
+                        value={this.state.article.name} 
+                        validators={['required']}
+                        errorMessages={['Veuillez saisir un nom d\'article']}
+                    />
 
-                     <input type="text"
-                     placeholder="Nom de l'article" 
-                     className="form-control"
-                     value={this.state.name} 
-                     onChange={this.handleChangeName}/>
-
-                     <input type="number" 
-                     placeholder="Quantité" 
-                     className="form-control"
-                     value={this.state.quantity} 
-                     onChange={this.handleChangeQuantity} />
-
+                    <TextValidator
+                        type="number" 
+                        placeholder="Quantité" 
+                        className="form-control"
+                        name="itemQuantity"
+                        value={this.state.article.quantity} 
+                        onChange={this.handleChangeQuantity}
+                        validators={['required']}
+                        errorMessages={['Veuillez saisir une quantité']}
+                    />
                      <button type="submit" className="btn btn-secondary">Ajouter</button>
-                 </form>
+                 </ValidatorForm>
              </div>
          );
      }
 
-     handleSubmit = (event) => {
-         this.props.addArticle(this.state);
-         this.setState({...this.DEFAULT_STATE});
-         event.preventDefault();
-     };
-
-     handleChangeQuantity = (event) => {
-         this.setState({quantity: event.target.value});
+     handleSubmit(event) {
+        event.preventDefault();
+        if (true) {
+            
+        }
+        this.props.addArticle(this.state.article);
+        this.setState({...this.DEFAULT_STATE});
+    
      }
-     handleChangeName = (event) => {
-         this.setState({name: event.target.value});
+    
+     handleChangeQuantity(event) {
+         this.setState(
+             {
+                 ...this.state, 
+                 article: {
+                     ...this.state.article, 
+                     quantity: event.target.value
+                    }
+            }
+        );
+     }
+
+     handleChangeName(event){
+        this.setState(
+            {
+                ...this.state, 
+                article: {
+                    ...this.state.article, 
+                    name: event.target.value
+                   }
+           }
+       );
      }
 }
 
